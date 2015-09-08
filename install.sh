49f4e43b6d1869e855508b8359cefd3e25fc68cb
ryantownshend <ryan@discoverygarden.ca>
2015-09-08 14:27:22 -0300

    rebuild of install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,35 @@
+#!/bin/bash
 # installer script for project_zebu vim config
 
 ### WARNING, work in progress, do not use
 
 # vars
-bundle_path="~/.vim/bundle"
-zebu="~/.project_zebu"
+BUNDLE_PATH="$HOME/.vim/bundle"
+ZEBU="$HOME/.project_zebu"
 
 
 # verify ~/.vim/bundles exists
-if [ ! -e "$bundle_path" ]; then
-    mkdir -p "$bundle_path"
+if [ ! -d "$BUNDLE_PATH" ]; then
+    echo $BUNDLE_PATH
+    mkdir -p "$BUNDLE_PATH"
 fi
 
 # install vundle
-git clone https://github.com/VundleVim/Vundle.vim.git $bundle_path/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git "$BUNDLE_PATH/Vundle.vim"
 
 # verify ~/.project_zebu exists
-if [ ! -e "$zebu" ]; then
-    mkdir =p "$zebu"
+if [ ! -d "$ZEBU" ]; then
+    echo $ZEBU
+    mkdir -p "$ZEBU"
 fi
 
 # clone the project itself
-git clone https://github.com/ryantownshend/project_zebu.git $zebu
+git clone https://github.com/ryantownshend/project_zebu.git "$ZEBU"
 
 # plant the symlinks in home dir
-ln -s .project_zebu/.vimrc ~/.vimrc
-ln -s .project_zebu/.vimrc.bundles ~/.vimrc.bundles
+ln -s "$ZEBU/.vimrc" "$HOME/.vimrc"
+ln -s "$ZEBU/.vimrc.bundles" "$HOME/.vimrc.bundles"
 
 # run Vundle plugin installs
 vim +PluginInstall +qall
+
